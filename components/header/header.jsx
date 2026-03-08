e7058e1f87d41fae5a9f864511b1d66056348117
Harrison Syfert <harrisonsyfert@gmail.com>
2026-03-08 14:44:09 -0400

    Add Header component with title and tagline

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import './header.css';
 
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>My React Project</h1>
+        <p>Code. Create. Innovate 🚀</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
