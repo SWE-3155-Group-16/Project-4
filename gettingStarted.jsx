178207973c9cac8c44a2650a1acff2b7188bdf0c
Harrison Syfert <harrisonsyfert@gmail.com>
2026-03-08 14:44:57 -0400

    Import Header component in gettingStarted.jsx

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
-
+import Header from './components/header/header';
 import Example from './components/example/Example';
 
 ReactDOM.render(
